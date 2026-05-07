cbf2c7d6c01bd9d8ba2352b9f8b572c2cd24fc7f
dpetiukov <dpetiukov@gmail.com>
2026-05-07 20:11:32 +0300

    Add project files.

---
+++ PV511-T33-Exception/MobileException.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+using std::string;
+
+class MobileException {
+protected:
+	string text;
+public:
+	MobileException(string text) {
+		this->text = text;
+	}
+	virtual~MobileException() {};
+	virtual string showMessage()const = 0;
+};
+
+class NameException : public MobileException {
+protected:
+	string name;
+public:
+	NameException(string text, string name) : MobileException(text), name(name) {
+
+	}
+	virtual string showMessage()const override {
+		return "NameException: " + text + " = " + name;
+	}
+};
